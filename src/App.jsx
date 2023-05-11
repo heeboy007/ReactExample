c893dcccf49c1afeee8d913d48ddad0e2a1f81ac
geezhardname <15987265-geezhardname@users.noreply.replit.com>
2023-05-11 14:11:16 +0000

    16-2 state 설정(mode 프로퍼티, welcom 프로퍼티) - state의 값이 바뀔때 마다 render 재호출!

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode: 'welcome',
       subject: {title:'WEB', sub:'World Wide Web!'},
+      welcome: {title:'Welcome', desc:'Hello, React!!'},
       contents: [
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'}
