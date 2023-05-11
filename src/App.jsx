640fefde150a3e060962a530d652bb05158ce2d0
geezhardname <15987265-geezhardname@users.noreply.replit.com>
2023-05-11 13:18:33 +0000

    11-2 Subject 컴포넌트 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
+      </header>
+    );
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="/public/pure.html"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+        <Subject></Subject>
       </div>
     );
   }
