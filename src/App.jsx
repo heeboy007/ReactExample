ad3ca2046f508ab24b61bc68c85a47267e6dad64
geezhardname <15987265-geezhardname@users.noreply.replit.com>
2023-05-11 13:51:30 +0000

    15-3 state에 content 프로퍼티 추가

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,12 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      subject: {title:'WEB', sub:'World Wide Web!'}
+      subject: {title:'WEB', sub:'World Wide Web!'},
+      contents: [
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'Javascript is for interactive'},
+      ]
     }
   }
   
