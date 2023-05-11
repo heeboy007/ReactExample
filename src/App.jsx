d9f23810afc9ad4e669222503318eac4b38a5c62
geezhardname <15987265-geezhardname@users.noreply.replit.com>
2023-05-11 13:49:00 +0000

    15-2 state 값으로 Subject 컴포넌트의 props 값 설정

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}>
+        </Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
