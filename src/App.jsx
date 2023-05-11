c1b05309dac5fcd9b8edd5f060abd1daf776bd2d
geezhardname <15987265-geezhardname@users.noreply.replit.com>
2023-05-11 13:15:05 +0000

    11-1 logo를 임포트하는 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../public/favicon.svg';
 import './App.css';
 
 class App extends Component {
@@ -13,7 +12,7 @@ class App extends Component {
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="/public/pure.html"
             target="_blank"
             rel="noopener noreferrer">
             Learn React
