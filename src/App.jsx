fee313f63c9ff643dd4185a3a5af64a6383454f1
geezhardname <15987265-geezhardname@users.noreply.replit.com>
2023-05-11 13:58:13 +0000

    15-4 TOC 컴포넌트에 data props로 contents 프로퍼티 주입

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ class App extends Component {
           title={this.state.subject.title} 
           sub={this.state.subject.sub}>
         </Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
