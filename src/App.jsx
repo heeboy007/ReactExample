262ec07d2f486de0d2464b37bb542aac9c29f916
geezhardname <heeboy007@naver.com>
2023-11-14 14:14:16 +0000

    END OF CHAPTER 1

--- src/App.jsx
+++ src/App.jsx
@@ -103,9 +103,28 @@ class App extends Component {
 				});
 			}.bind(this)}></TOC>
 		<Control onChangeMode={function(_mode){
-				this.setState({
-					mode:_mode
-				});
+				if(_mode === 'delete'){
+					if(window.confirm('그렇다면 파괴합니다.')){
+						var _contents = Array.from(this.state.contents);
+						var i = 0;
+						while(i < _contents.length){
+							if(_contents[i].id == this.state.selected_content_id){
+								_contents.splice(i, 1);
+								break;
+							}
+							i = i + 1;
+						}
+						this.setState({
+							mode:'welcome',
+							contents: _contents
+						})
+						alert('마치 존재하지 않았던 것처럼.');
+					}
+				} else {
+					this.setState({
+						mode:_mode
+					});	
+				}
 			}.bind(this)}></Control>
         {this.getContent()}
       </div>
