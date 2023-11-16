8ee342d1fa5d338728139db3ccf791f16b2dd3a7
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-11-16 20:52:46 +0900

    3-2 react-router-dom changed quite a bit

--- reactrouterdomexample/src/App.jsx
+++ reactrouterdomexample/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 function Home() {
   return (
@@ -33,9 +34,16 @@ function App() {
   return (
     <div className="App">
       <h1>Hello React Router</h1>
-      <Home></Home>
-      <Topics></Topics>
-      <Contact></Contact>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/topics">Topics</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/topics" Component={Topics} />
+        <Route path="/contact" Component={Contact} />
+      </Routes>
     </div>
   );
 }
