f8e1d72d3b3e2b37750c6fbbe41b09447f529166
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-11-16 21:07:08 +0900

    4-2 주석 추가

--- reactrouterdomexample/src/index.jsx
+++ reactrouterdomexample/src/index.jsx
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App.jsx';
 import { BrowserRouter, HashRouter } from 'react-router-dom';
 
+//웹 서버 설정에 따라, 어떤 패스로 들어오든 루트 페이지에 있는 HTML을 서비스할 수 있을 경우 Browser,
+//아니면 HashRouter를 쓰면 된다.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter><App /></HashRouter>
