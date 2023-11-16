657880c87dda83ad6e5a2785ea627b2cb8195044
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-11-16 12:08:09 +0900

    2-3 App 컴포넌트에서 사용할 3개의 컴포넌트 만들기

--- reactrouterdomexample/src/App.jsx
+++ reactrouterdomexample/src/App.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import './App.css';
 
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      Home..
+    </div>
+  );
+}
+
+function Topics() {
+  return (
+    <div>
+      <h2>Topics</h2>
+      Topics..
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      Contact..
+    </div>
+  );
+}
+
+
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <h1>Hello React Router</h1>
+      <Home></Home>
+      <Topics></Topics>
+      <Contact></Contact>
     </div>
   );
 }
