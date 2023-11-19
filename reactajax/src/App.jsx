ca2bcf8e54951de83dfaaade184e52dad64dfe7c
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-11-19 21:02:51 +0900

    2-3 불필요한 코드 제거

--- reactajax/src/App.jsx
+++ reactajax/src/App.jsx
@@ -3,7 +3,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <h1>WEB</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href='1'>HTML</a>
+            <a href='2'>CSS</a>
+            <a href='3'>React</a>
+          </li>
+        </ul>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, React &amp; Ajax
+      </article>
     </div>
   );
 }
