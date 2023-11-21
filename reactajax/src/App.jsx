4204b6b733813c4701079050f263402e12ee601d
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-11-21 23:29:42 +0900

    3-3 자바스크립트 객체로 Nav 컴포넌트의 리스트 작성

--- reactajax/src/App.jsx
+++ reactajax/src/App.jsx
@@ -2,23 +2,32 @@ import { Component } from 'react';
 import './App.css';
 
 class Nav extends Component {
+  state = {
+    list: []
+  }
+
   componentDidMount(){
     fetch('list.json')
       .then(function(result){
         return result.json();
       })
       .then(function(json){
-        console.log(json);
+        this.setState({list:json});
       }.bind(this));
+    //가져온 값은 state라는 좋은 재렌더링 + 저장 기능을 통해서
+    //렌더링을 게시하자. 바로 렌더링에 영향을 주는 것은 좋지 않다.
   }
 
   render() {
+    var listTag = [];
+    for(var i = 0; i < this.state.list.length; i++){
+      var li = this.state.list[i];
+      listTag.push(<li key={li.id}><a href={li.id}>{li.title}</a></li>);
+    }
     return(
       <nav>
         <ul>
-          <li><a href='1'>HTML</a></li>
-          <li><a href='2'>CSS</a></li>
-          <li><a href='3'>React</a></li>
+          {listTag}
         </ul>
       </nav>
     );
