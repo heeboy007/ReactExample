5077dd0bd24f4d5613f3c3b6a720fa474eb22656
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-11-21 23:24:33 +0900

    3-1~2 Nav컴포넌트 분리, fetch를 이용한 json 가져오기

--- reactajax/src/App.jsx
+++ reactajax/src/App.jsx
@@ -1,18 +1,35 @@
+import { Component } from 'react';
 import './App.css';
+
+class Nav extends Component {
+  componentDidMount(){
+    fetch('list.json')
+      .then(function(result){
+        return result.json();
+      })
+      .then(function(json){
+        console.log(json);
+      }.bind(this));
+  }
+
+  render() {
+    return(
+      <nav>
+        <ul>
+          <li><a href='1'>HTML</a></li>
+          <li><a href='2'>CSS</a></li>
+          <li><a href='3'>React</a></li>
+        </ul>
+      </nav>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <h1>WEB</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='1'>HTML</a>
-            <a href='2'>CSS</a>
-            <a href='3'>React</a>
-          </li>
-        </ul>
-      </nav>
+      <Nav></Nav>
       <article>
         <h2>Welcome</h2>
         Hello, React &amp; Ajax
