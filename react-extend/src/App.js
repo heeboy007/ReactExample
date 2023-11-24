529e6b0b8292d9fe5eb50effa5f36fc60df82d0e
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-11-24 21:46:13 +0900

    2-4-2 기본 연산자 이용법

--- react-extend/src/App.js
+++ react-extend/src/App.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from "react";
 
 function App() {
+  const name = '리액트';
   return (
     <>
       <h1>리액트 안녕!</h1>
       <h2>잘 작동하니?</h2>
+      <p>아래는 삼항연산자</p>
+      {name === '리액트' ? <h1>리액트입니다.</h1> : <h2>리액트가 아닙니다.</h2>}
+      <p>아래는 AND 연산자</p>
+      {name === '리액트' && <h1>리액트입니다.</h1>}
     </>
   );
 }
