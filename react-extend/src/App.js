586389024cc8eeca4e89ecfcfe2a0377dac2483e
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-11-24 21:48:32 +0900

    2-4-6 인라인 스타일링

--- react-extend/src/App.js
+++ react-extend/src/App.js
@@ -2,15 +2,25 @@ import React, { Fragment } from "react";
 
 function App() {
   const name = '리액트';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    fontSize: '48px',
+    fontWeight: 'bold',
+    padding: 16 //단위를 생략하는 pxㄹ 지정됩니다.
+  };
   return (
-    <>
+    <div>
       <h1>리액트 안녕!</h1>
       <h2>잘 작동하니?</h2>
       <p>아래는 삼항연산자</p>
       {name === '리액트' ? <h1>리액트입니다.</h1> : <h2>리액트가 아닙니다.</h2>}
       <p>아래는 AND 연산자</p>
       {name === '리액트' && <h1>리액트입니다.</h1>}
-    </>
+      <div style={style}>
+        스타일이 적용된! {name}
+      </div>
+    </div>
   );
 }
 
