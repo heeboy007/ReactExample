af7d565aa93b1322d11455860ff7a2e4784ee926
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-11-27 11:14:11 +0900

    4-2-3 임의 메서드 만들어서 함수전달하기(오류)

---
+++ react-extend/src/04/App04.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import './App04.css';
+import EventPractice from "./src/EventPractice";
+
+class App04 extends Component {
+    render() {
+        return (
+            <div>
+                <EventPractice></EventPractice>
+            </div>
+        );
+    }
+}
+
+export default App04;
--- react-extend/src/App.js
+++ react-extend/src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 //각 챕터 별 소스앱 임포트
 import App02 from './02/App02';
 import App03 from './03/App03';
+import App04 from './04/App04';
 
 function BaseLinks() {
   return (
@@ -12,6 +13,7 @@ function BaseLinks() {
       <ul>
         <li><Link to="/02">Chapter 02</Link></li>
         <li><Link to="/03">Chapter 03</Link></li>
+        <li><Link to="/04">Chapter 04</Link></li>
       </ul>
     </div>
   );
@@ -23,6 +25,7 @@ function App() {
       <Route exact path="/" Component={BaseLinks}></Route>
       <Route path="/02" Component={App02}></Route>
       <Route path="/03" Component={App03}></Route>
+      <Route path="/04" Component={App04}></Route>
     </Routes>
   );
 }
