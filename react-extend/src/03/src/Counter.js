f51dfabcccee17a0cd0166e01be6f0c4493ebf71
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2023-11-27 10:56:26 +0900

    3-4-1-3 setState에 객체 대신 함수 인자 전달하기

--- react-extend/src/03/src/Counter.js
+++ react-extend/src/03/src/Counter.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            number: 0
-        };
-    }
+    state = {
+        number: 0,
+        fixedNumber: 0
+    };
     render() {
-        const { number } = this.state;
+        const { number, fixedNumber } = this.state;
         return(
             <div>
                 <h1>{number}</h1>
+                <h3>건드려지지 않는 숫자 : {fixedNumber}</h3>
                 <button onClick={(e) => {
-                    e.preventDefault();
-                    this.setState({ number: number + 1 });
+                    this.setState(prevState => {
+                        return { number: prevState.number + 1 }
+                    });
                 }}>
                     +1
                 </button>
